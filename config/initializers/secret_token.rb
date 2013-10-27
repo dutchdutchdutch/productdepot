bbc28950a0d9ac196dd9cee769d9b2d1b6edbc43
dutchdutchdutch <dutchsteutel@gmail.com>
2013-10-26 20:32:51 -0700

    Product Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Productdepot::Application.config.secret_key_base = '3ff36fb1efc6012f3bbd83a1e8a335ff8e8741a323f795242f44aecff8776aec5fdf1c1a5c3a586785ad296dfb6ece9a24b0e0e1f91f12d615fd0059a0bfd212'
